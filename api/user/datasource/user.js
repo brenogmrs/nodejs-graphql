6989783068436ae3e24fe40bab34d9306e8623fd
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-02-10 09:52:27 -0300

    feat: add role to users response

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -7,7 +7,15 @@ class UsersAPI extends RESTDataSource {
   }
 
   async getAllUsers() {
-    return this.get('/users');
+    const users = await this.get('/users');
+
+    return users.map(async (user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      active: user.active,
+      role: await this.get(`/roles/${user.role}`),
+    }));
   }
 
   async getUserById(id) {
